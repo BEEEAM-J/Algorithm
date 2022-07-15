07c7b1f429befe49a922a3766cd2a868f2af474c
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2022-07-15 15:06:17 +0900

    백준 10872 (팩토리얼)

--- baekjoon.py
+++ baekjoon.py
@@ -1,22 +1,8 @@
-def func(x):
-    global counter
-    if(len(x) > 1):             # x가 한자리 수가 아닐때
-        counter += 1
-        y = 0
-        for i in x:
-            y += int(i)         # x의 각 자리의 수를 단순히 더한 수 y를 만든다.
-        func(str(y))
-    
-    else:                       # x가 한자리 수이면 3의배수인지 확인한다
-        x = int(x)
-        if(x % 3 == 0):
-            print(counter)
-            print("YES")
-        else:
-            print(counter)
-            print("NO")
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-counter = 0
-
-num = input()
-func(num)
+num = int(input())
+print(factorial(num))
