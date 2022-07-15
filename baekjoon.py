97669d72f5e9e977974272a985d581375c2045ab
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2022-07-15 15:09:00 +0900

    백준 2747 (피보나치 수열)

--- baekjoon.py
+++ baekjoon.py
@@ -1,8 +1,16 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+dictionary = {                                          # 딕셔너리 없이 사용하면 시간 초과 생김
+        0: 0,
+        1: 1
+    }
 
+def fibo(n):
+    if n in dictionary:         
+        return dictionary[n]
+    else:
+        res = fibo(n - 1) + fibo(n - 2)     
+        dictionary[n] = res                             # 한번 계산했던 값을 딕셔너리에 저장하여 불필요한 계산 횟수 감소
+        return res
+    
 num = int(input())
-print(factorial(num))
+print(fibo(num))
+
