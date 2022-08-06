b701cc4b286b279df3f775b412159287dd857f57
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2022-08-06 17:08:28 +0900

    dfs 알고리즘 구현

--- baekjoon.py
+++ baekjoon.py
@@ -1,22 +1,25 @@
-def func(g, idx, left, right):
+def dfs(graph, start, visited = []):
+    visited.append(start)                   # 방문 처리
+    # print(visited)                          # 추가되는 순서 보여줌
+
+    for node in graph[start]:               # 인접 노드 확인
+        if node not in visited:             # 인접 노드들 중에서 방문하지 않은것 찾기
+            dfs(graph, node, visited)
+            
+    return visited
     
-    value = abs(left - right)
 
-    if value not in list_pos:
-        list_pos.append(value)
+graph = dict()                      
+ 
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
 
-    if idx + 1 == k:
-        return 0
-
-    if idx != k:
-        func(g, idx + 1, left + g[idx + 1], right)      # 추를 좌측에 놓을 때
-        func(g, idx + 1, left, right + g[idx + 1])      # 추를 우측에 놓을 때
-        func(g, idx + 1, left, right)   
-
-
-k = int(input())
-g = list(map(int, input().split()))
-list_pos = [0]
-s = sum(g)
-func(g, -1, 0, 0)
-print((s + 1) - len(list_pos))
+print(dfs(graph, 'A'))
