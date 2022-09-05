5946db5f19d7907827e83c67d7de602ef6c2b0d3
조범준 <107917980+BeamjunCho9@users.noreply.github.com>
2022-09-05 17:37:52 +0900

    dfs 알고리즘

---
+++ dfs 알고리즘.py
@@ -0,0 +1,25 @@
+def dfs(graph, start, visited = []):
+    visited.append(start)                   # 방문 처리
+    # print(visited)                          # 추가되는 순서 보여줌
+
+    for node in graph[start]:               # 인접 노드 확인
+        if node not in visited:             # 인접 노드들 중에서 방문하지 않은것 찾기
+            dfs(graph, node, visited)
+
+    return visited
+
+
+graph = dict()                      
+
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+print(dfs(graph, 'A'))
