9ce2d562f4db48c1a93e03e4edaabace0c9ffa27
조범준 <107917980+BEEEAM-J@users.noreply.github.com>
2023-11-02 17:25:28 +0900

    [Silver III] Title: 통계학, Time: 1800 ms, Memory: 83368 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -6,16 +6,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();       // {key: 숫자, value: 빈도}
         int N = Integer.parseInt(br.readLine());
         int i, inputNum;
         int avg, mid, range;
+        int total = 0;
 
         List<Integer> numbers = new ArrayList<>();
         List<Integer> frequencyList = new ArrayList<>();
 
         for (i = 0; i < N; i ++) {
             inputNum = Integer.parseInt(br.readLine());
+            total += inputNum;
             numbers.add(inputNum);
             if (frequencyMap.containsKey(inputNum)) {
                 frequencyMap.put(inputNum, frequencyMap.get(inputNum) + 1);
@@ -27,12 +29,12 @@ public class Main {
 
         Collections.sort(numbers);
 
-        avg = (int) Math.round(((double) (numbers.stream().mapToInt(Integer::intValue).sum()) / N));
-        mid = numbers.get(N / 2);
-        range = Collections.max(numbers) - Collections.min(numbers);
+        avg = (int) Math.round(((double) total / N)); // 산술 평균 (반올림)
+        mid = numbers.get(N / 2);   // 중앙 값
+        range = Collections.max(numbers) - Collections.min(numbers); // 범위
 
-        for (int key: frequencyMap.keySet()) {
-            if (frequencyMap.get(key) == Collections.max(frequencyMap.values())) {      // key로 가져온 map의 value와 value의 최대값 비교, 같으면 최빈값
+        for (int key: frequencyMap.keySet()) {  // map의 key들로 반복
+            if (frequencyMap.get(key) == Collections.max(frequencyMap.values())) {      // key로 가져온 map의 value(빈도)와 value(빈도)의 최대값 비교, 같으면 최빈값
                 frequencyList.add(key);
             }
         }
