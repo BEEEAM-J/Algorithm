62da333cd577bd7f002d493d7c0896f1d8b03d73
조범준 <107917980+BEEEAM-J@users.noreply.github.com>
2023-11-09 17:55:06 +0900

    [level 2] Title: 가장 큰 수, Time: 2.11 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,26 +1,28 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class Solution {
     public String solution(int[] numbers) {
-        
-        String[] arr = new String[numbers.length];
-        StringBuilder sb = new StringBuilder();
         String answer = "";
-        
-        for (int i = 0; i < numbers.length; i++) {
-            arr[i] = Integer.toString(numbers[i]);
+
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < numbers.length; i++) {
+            list.add(numbers[i]);
         }
-        
-        Arrays.sort(arr, (n1, n2) -> (n2 + n1).compareTo(n1 + n2));
-        
-        for (String str: arr) {
-            sb.append(str);
+        Collections.sort(list, (a, b) -> {
+            String as = String.valueOf(a), bs = String.valueOf(b);
+            return -Integer.compare(Integer.parseInt(as + bs), Integer.parseInt(bs + as));
+        });
+        StringBuilder sb = new StringBuilder();
+        for(Integer i : list) {
+            sb.append(i);
         }
-        
         answer = sb.toString();
-        
-        if(answer.charAt(0) == '0') answer = "0";
-        
-        return answer;
+        if(answer.charAt(0) == '0') {
+            return "0";
+        }else {
+            return answer;
+        }
     }
 }
